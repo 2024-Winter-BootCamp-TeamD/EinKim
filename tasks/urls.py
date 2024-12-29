b0dcbd8783889e7fa9997eb573221bcd5c864d28
einhn <ainkm@naver.com>
2024-12-30 01:39:26 +0900

    Swagger documentation and security patch

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import TaskListView, TaskDetailView
 
 urlpatterns = [
+    path('tasks/', TaskListView.as_view(), name='task-list'),
+    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
     path('', views.task_list, name='task-list'),
     path('create/', views.create_task, name='create-task'),
     path('update/<int:pk>/', views.edit_task, name='edit-task'),
