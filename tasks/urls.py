ac0e11506eb7b4e570e52912dc0217abec89bac4
einhn <ainkm@naver.com>
2024-12-29 16:21:11 +0900

    Save progress before switching branches

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.task_list, name='task-list'),
+    path('create/', views.create_task, name='create-task'),
+    path('update/<int:pk>/', views.update_task, name='update-task'),
+    path('delete/<int:pk>/', views.delete_task, name='delete-task'),
+
 ]
